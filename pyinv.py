e54da6bf4fe6d8c3886f45703d2bc0b409f32f50
JulioPDX <juliop503@gmail.com>
2023-11-29 10:49:40 -0800

    minor edits to fix validate input

--- pyinv.py
+++ pyinv.py
@@ -1,3 +1,5 @@
+import copy
+import sys
 import time
 from ansible.inventory.manager import InventoryManager
 from ansible.parsing.dataloader import DataLoader
@@ -9,7 +11,7 @@ from pyavd import (
     get_device_config,
     validate_inputs,
 )
-# from rich import print, inspect
+from rich import print
 
 
 dl = DataLoader()
@@ -20,7 +22,7 @@ hosts = {}
 
 for host in im.get_hosts("all"):
     host_vars = vm.get_vars(host=host)
-    hosts[str(host)] = host_vars
+    hosts[str(host)] = copy.deepcopy(host_vars)
 
 with Progress() as progress:
     total = len(hosts)
@@ -35,10 +37,13 @@ with Progress() as progress:
 
     for k, v in hosts.items():
         validate = validate_inputs(v)
+        if validate.failed:
+            print(f"The following validation errors were seen for {k}")
+            for issue in validate.validation_errors:
+                print(issue)
+            sys.exit(1)
         progress.update(task2, advance=1)
         time.sleep(0.5)
-        # print(k)
-        # inspect(validate)
 
         struct_conf = get_device_structured_config(k, v, facts)
         progress.update(task3, advance=1)
