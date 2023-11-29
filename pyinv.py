226002090c7c26cc58e749c7b1bd4fc74a9c403f
JulioPDX <juliop503@gmail.com>
2023-11-28 22:49:50 -0800

    minor edits to flow

--- pyinv.py
+++ pyinv.py
@@ -3,7 +3,13 @@ from ansible.inventory.manager import InventoryManager
 from ansible.parsing.dataloader import DataLoader
 from ansible.vars.manager import VariableManager
 from rich.progress import Progress
-from pyavd import get_avd_facts, get_device_structured_config, get_device_config
+from pyavd import (
+    get_avd_facts,
+    get_device_structured_config,
+    get_device_config,
+    validate_inputs,
+)
+# from rich import print, inspect
 
 
 dl = DataLoader()
@@ -18,21 +24,28 @@ for host in im.get_hosts("all"):
 
 with Progress() as progress:
     total = len(hosts)
-    task1 = progress.add_task("[red]facts...", total = total)
-    task2 = progress.add_task("[yellow]structured config...", total = total)
-    task3 = progress.add_task("[green]build config...", total = total)
+    task1 = progress.add_task("[red]facts...", total=1)
+    task2 = progress.add_task("[blue]validate inputs...", total=total)
+    task3 = progress.add_task("[yellow]structured config...", total=total)
+    task4 = progress.add_task("[green]build config...", total=total)
+
+    facts = get_avd_facts(hosts)
+    progress.update(task1, advance=1)
+    time.sleep(0.5)
 
     for k, v in hosts.items():
-        facts = get_avd_facts(hosts)
-        progress.update(task1, advance=1)
-        time.sleep(.5)
+        validate = validate_inputs(v)
+        progress.update(task2, advance=1)
+        time.sleep(0.5)
+        # print(k)
+        # inspect(validate)
 
         struct_conf = get_device_structured_config(k, v, facts)
-        progress.update(task2, advance=1)
-        time.sleep(.5)
+        progress.update(task3, advance=1)
+        time.sleep(0.5)
 
         config = get_device_config(struct_conf)
         with open(f"py_conf/{k}.cfg", "w") as f:
             f.writelines(config)
-        progress.update(task3, advance=1)
-        time.sleep(.5)
+        progress.update(task4, advance=1)
+        time.sleep(0.5)
